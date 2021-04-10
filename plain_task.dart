7a51cbd894d00d5b76725a4fb2b5a9abba93ed43
andrew <andrew_mokryj@ukr.net>
2021-04-10 18:18:55 +0300

    Add task classes

--- plain_task.dart
+++ plain_task.dart
@@ -1,5 +1,14 @@
 import 'task.dart';
 
 class PlainTask implements Task {
-  
+  final int id;
+  String name;
+  String category;
+
+  PlainTask({this.id, this.name, this.category});
+
+  @override
+  String toString() {
+    return "Plain Task: id = $id, name = $name, category = $category";
+  }
 }
